d3a9a78ba367f2030ecc691d893c9702041eb464
Tracey <radcliffe.tracey@gmail.com>
2016-07-17 13:55:56 -0700

    passed tests

--- test/bitmap_palette_test.js
+++ test/bitmap_palette_test.js
@@ -10,4 +10,10 @@ describe('get the size of the bitmap file', () => {
     expect(bitmap.bmpSize).to.eql(11078);
     expect(bitmap.pixelArrayStartLoc).to.eql(1078);
   });
+  it('is checking to make sure it is a function', () =>{
+    expect(bitmap.transformPalletteBitmap).to.be.a('function');
+  });
+  it('making sure the image was altered', () =>{
+    expect('../palette-bitmap.bmp').to.not.eql('../converted.bmp');
+  });
 });
