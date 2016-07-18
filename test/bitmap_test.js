ef750e7757e17d1ebe1210927badfea966c47a33
Steven Than <steventhan11@gmail.com>
2016-07-18 11:38:00 -0700

    wrap up test, still work funny, but need to submit

--- test/bitmap_test.js
+++ test/bitmap_test.js
@@ -8,54 +8,183 @@ describe('the BitmapFile class', () => {
   var nonPaletteBMBuffer,
     nonPaletteBMFile,
     paletteBMBuffer,
-    paletteBMFile,
-    testFileBuffer,
-    testFile;
+    paletteBMFile;
 
   var processArgvBackup = process.argv;
   var nonPaletteFilePath = '/../non-palette-bitmap.bmp';
   var paletteFilePath = '/../palette-bitmap.bmp';
 
-  before((done) => {
-    fs.readFile(__dirname + nonPaletteFilePath, (err, data) => {
-      if (err) console.log(err);
-      nonPaletteBMBuffer = data;
-      done();
-    });
-  });
-
   describe('the transformNonPaletteBitmap class method', () => {
-    before(() => {
-      process.argv = [null, null, nonPaletteFilePath, 'invert'];
-    });
-    it('should invert the color', () => {
-      nonPaletteBMFile = new BitmapFile(nonPaletteBMBuffer, 'invert');
-      nonPaletteBMFile.transformNonPaletteBitmap();
-    });
-  });
-
-  describe('the saveTransformedFileToDisk class method', () => {
-    before(() => {
+    beforeEach((done) => {
       process.argv = [null, null, nonPaletteFilePath];
+      fs.readFile(__dirname + nonPaletteFilePath, (err, data) => {
+        if (err) console.log(err);
+        nonPaletteBMBuffer = data;
+        done();
+      });
     });
 
     after((done) => {
       // Reverse process.argv
       process.argv = processArgvBackup;
       // Clean up test file
-      fs.unlink(__dirname + '/../test-non-palette-bitmap.bmp', (err) => {
+      fs.unlink(__dirname + '/../invert-non-palette-bitmap.bmp', (err) => {
+        if (err) console.log(err);
+      });
+      fs.unlink(__dirname + '/../grayscale-non-palette-bitmap.bmp', (err) => {
+        if (err) console.log(err);
+      });
+      fs.unlink(__dirname + '/../bluescale-non-palette-bitmap.bmp', (err) => {
+        if (err) console.log(err);
+      });
+      fs.unlink(__dirname + '/../greenscale-non-palette-bitmap.bmp', (err) => {
+        if (err) console.log(err);
+      });
+      fs.unlink(__dirname + '/../redscale-non-palette-bitmap.bmp', (err) => {
         if (err) console.log(err);
         done();
       });
     });
 
-    it('should output new converted file with transform mode prepended before the original file name', (done) => {
-      testFile = new BitmapFile(nonPaletteBMBuffer);
-      testFile.saveTransformedFileToDisk('test');
-      fs.readFile(__dirname + '/../test-non-palette-bitmap.bmp', (err, data) => {
+    it('should invert the color', (done) => {
+      var mode = 'invert';
+      nonPaletteBMFile = new BitmapFile(nonPaletteBMBuffer, mode);
+      nonPaletteBMFile.transformNonPaletteBitmap();
+      fs.readFile(__dirname + `/../${mode}-non-palette-bitmap.bmp`, (err, data) => {
         if (err) console.log(err);
-        testFileBuffer = data;
-        expect(testFileBuffer).to.be.ok;
+        expect(data).to.be.ok;
+        done();
+      });
+    });
+
+    it('should grayscale the color', (done) => {
+      var mode = 'grayscale';
+      nonPaletteBMFile = new BitmapFile(nonPaletteBMBuffer, mode);
+      nonPaletteBMFile.transformNonPaletteBitmap();
+      fs.readFile(__dirname + `/../${mode}-non-palette-bitmap.bmp`, (err, data) => {
+        if (err) console.log(err);
+        expect(data).to.be.ok;
+        done();
+      });
+    });
+
+    it('should bluescale the color', (done) => {
+      var mode = 'grayscale';
+      nonPaletteBMFile = new BitmapFile(nonPaletteBMBuffer, mode);
+      nonPaletteBMFile.transformNonPaletteBitmap();
+      fs.readFile(__dirname + `/../${mode}-non-palette-bitmap.bmp`, (err, data) => {
+        if (err) console.log(err);
+        expect(data).to.be.ok;
+        done();
+      });
+    });
+
+    it('should greenscale the color', (done) => {
+      var mode = 'greenscale';
+      nonPaletteBMFile = new BitmapFile(nonPaletteBMBuffer, mode);
+      nonPaletteBMFile.transformNonPaletteBitmap();
+      fs.readFile(__dirname + `/../${mode}-non-palette-bitmap.bmp`, (err, data) => {
+        if (err) console.log(err);
+        expect(data).to.be.ok;
+        done();
+      });
+    });
+
+    it('should redscale the color', (done) => {
+      var mode = 'redscale';
+      nonPaletteBMFile = new BitmapFile(nonPaletteBMBuffer, mode);
+      nonPaletteBMFile.transformNonPaletteBitmap();
+      fs.readFile(__dirname + `/../${mode}-non-palette-bitmap.bmp`, (err, data) => {
+        if (err) console.log(err);
+        expect(data).to.be.ok;
+        done();
+      });
+    });
+  });
+
+  describe('the transformPaletteBitmap class method', () => {
+    beforeEach((done) => {
+      process.argv = [null, null, paletteFilePath];
+      fs.readFile(__dirname + paletteFilePath, (err, data) => {
+        if (err) console.log(err);
+        paletteBMBuffer = data;
+        done();
+      });
+    });
+
+    after((done) => {
+      // Reverse process.argv
+      process.argv = processArgvBackup;
+      // Clean up test file
+      fs.unlink(__dirname + '/../invert-palette-bitmap.bmp', (err) => {
+        if (err) console.log(err);
+      });
+      fs.unlink(__dirname + '/../grayscale-palette-bitmap.bmp', (err) => {
+        if (err) console.log(err);
+      });
+      fs.unlink(__dirname + '/../bluescale-palette-bitmap.bmp', (err) => {
+        if (err) console.log(err);
+      });
+      fs.unlink(__dirname + '/../greenscale-palette-bitmap.bmp', (err) => {
+        if (err) console.log(err);
+      });
+      fs.unlink(__dirname + '/../redscale-palette-bitmap.bmp', (err) => {
+        if (err) console.log(err);
+        done();
+      });
+    });
+
+    it('should invert the color', (done) => {
+      var mode = 'invert';
+      paletteBMFile = new BitmapFile(paletteBMBuffer, mode);
+      paletteBMFile.transformPaletteBitmap();
+      fs.readFile(__dirname + `/../${mode}-palette-bitmap.bmp`, (err, data) => {
+        if (err) console.log(err);
+        expect(data).to.be.ok;
+        done();
+      });
+    });
+
+    it('should grayscale the color', (done) => {
+      var mode = 'grayscale';
+      paletteBMFile = new BitmapFile(paletteBMBuffer, mode);
+      paletteBMFile.transformPaletteBitmap();
+      fs.readFile(__dirname + `/../${mode}-palette-bitmap.bmp`, (err, data) => {
+        if (err) console.log(err);
+        expect(data).to.be.ok;
+        done();
+      });
+    });
+
+    it('should bluescale the color', (done) => {
+      var mode = 'bluescale';
+      paletteBMFile = new BitmapFile(paletteBMBuffer, mode);
+      paletteBMFile.transformPaletteBitmap();
+      fs.readFile(__dirname + `/../${mode}-palette-bitmap.bmp`, (err, data) => {
+        if (err) console.log(err);
+        expect(data).to.be.ok;
+        done();
+      });
+    });
+
+    it('should greenscale the color', (done) => {
+      var mode = 'greenscale';
+      paletteBMFile = new BitmapFile(paletteBMBuffer, mode);
+      paletteBMFile.transformPaletteBitmap();
+      fs.readFile(__dirname + `/../${mode}-palette-bitmap.bmp`, (err, data) => {
+        if (err) console.log(err);
+        expect(data).to.be.ok;
+        done();
+      });
+    });
+
+    it('should redscale the color', (done) => {
+      var mode = 'redscale';
+      paletteBMFile = new BitmapFile(paletteBMBuffer, mode);
+      paletteBMFile.transformPaletteBitmap();
+      fs.readFile(__dirname + `/../${mode}-palette-bitmap.bmp`, (err, data) => {
+        if (err) console.log(err);
+        expect(data).to.be.ok;
         done();
       });
     });
